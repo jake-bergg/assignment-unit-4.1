6f5f35b74f4a6f16bf85a377ef04fc604d6875b6
Jake Berg <jberg543@outlook.com>
2024-10-18 23:43:22 -0500

    questions 6-7

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -57,15 +57,27 @@ console.log('Is the number -1 a positive number?', isPositive(-1));
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast(array) {
-
+  if (array.length === 0) {
+    return undefined;
+  } else {
+    return array[array.length - 1];
+  }
 }
+console.log(getLast([1,2,3,4,5]));
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find(value, array) {
-
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return true; // Value found
+    }
+  }
+  return false; // Value not found
 }
+console.log(find(1, [1, 2, 3, 4, 5]));
+
 
 // ----------------------
 // Stretch Goals
